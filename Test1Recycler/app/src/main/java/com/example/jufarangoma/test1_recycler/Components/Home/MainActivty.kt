f7e5307f184c79ebbc30a1e1d501df418c162f82
jufarangoma <jufarangoma@gmail.com>
2018-07-17 21:35:24 -0500

    Hello World

--- Test1Recycler/app/src/main/java/com/example/jufarangoma/test1_recycler/Components/Home/MainActivty.kt
+++ Test1Recycler/app/src/main/java/com/example/jufarangoma/test1_recycler/Components/Home/MainActivty.kt
@@ -1,8 +1,18 @@
 package com.example.jufarangoma.test1_recycler.Components.Home
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.example.jufarangoma.test1_recycler.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivty: AppCompatActivity(){
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv_main.text = "Peguelo re duro"
+    }
 
 }
